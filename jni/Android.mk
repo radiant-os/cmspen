9915772eea0874e093207a8df8f4137cdfd5879d
fattire <f4ttire@gmail.com>
2015-02-07 20:04:45 -0800

    liblog dependency in build system

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,5 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_LDLIBS := -llog
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE    := libSPenEventInjector
+LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := EventInjector.c
 include $(BUILD_SHARED_LIBRARY)
